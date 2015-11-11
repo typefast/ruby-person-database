6a2691b71d9db2a2f4910d46a5ae951c211e51d6
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-11 11:21:49 +0000

    fing_person method, case statement,

--- database.rb
+++ database.rb
@@ -32,6 +32,44 @@ def add_person
   $db.execute("INSERT INTO people (name, job, gender, age) VALUES (?,?,?,?)", name, job, gender, age)
 end
 
+def find_person
+  puts "Enter name or ID of person to find:"
+  id = gets.chomp
+  
+  person = $db.execute("SELECT * FROM people WHERE name = ? OR id = ?", id, id.to_i).first
+  
+  unless person
+    puts "No result found"
+    return
+  end
+  
+  puts %q{Name: #{person['name']}
+  Job: #{person['job']}
+  Gender: #{person['gender']}
+  Age: #{person['age']}}
+end
+
+loop do
+  puts %q{Please select an option:
+    
+    1. Create people table
+    2. Add a person
+    3. Look for a person
+    4. Quit
+  }
+  
+  case gets.chomp
+  when '1'
+    create_table
+  when '2'
+    add_person
+  when '3'
+    find_person
+  when '4'
+    disconnect_and_quit
+  end
+end
+
 
 
 
